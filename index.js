306fa5f7e61f32146b97f3a01d697b30a2739913
Anish Sinha <anishsinha0128@gmail.com>
2021-06-03 12:28:08 -0400

    initial changes

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const converter = function (sentence) {
+  let capitalized = [];
+  let words = sentence.split(" ");
+  words.forEach((word) => {
+    let capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1);
+    capitalized.push(capitalizedWord);
+  });
+  let converted = capitalized.join(" ");
+  return converted;
+};
+
+module.exports = converter;
